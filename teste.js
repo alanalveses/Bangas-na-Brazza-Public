b67df17472bdb9c9e0502a274e9492d723d76a27
Alan Alves <alan.alveses@gmail.com>
2024-04-07 18:59:11 -0300

    adds styling and content updates to pages

--- teste.js
+++
@@ -1,8 +0,0 @@
-function showCustomAlert() {
-    document.getElementById('customAlert').style.display = 'block';
-  }
-  
-  function closeCustomAlert() {
-    document.getElementById('customAlert').style.display = 'none';
-  }
-  
